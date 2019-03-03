91f965c56e4cc0c1555e3e09fd4cb198a86277e2
Daniel <danielko2001@gmail.com>
2019-03-03 13:43:34 -0600

    during writing session

--- src/HashTable.java
+++ src/HashTable.java
@@ -16,6 +16,7 @@
 //       and one of the techniques presented in lecture
 //
 /**
+ * TODO: talk about O(1) and O(N) and shit 
  * @author Daniel Ko
  *
  * @param <K>
@@ -48,6 +49,7 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 	 * @param initialCapacity
 	 * @param loadFactorThreshold
 	 */
+	// TODO: wtf about initial capacity, loadfactorthreshold. throw exception?
 	public HashTable(int initialCapacity, double loadFactorThreshold) {
 		// Initialize instance variables
 		tableSize = initialCapacity;
@@ -55,52 +57,63 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 		table = new hashPair[tableSize];
 	}
 
-	@Override 
+	@Override
 	public void insert(K key, V value) throws IllegalNullKeyException, DuplicateKeyException {
 		// Check if key is not null
-		if(key == null) {
+		if (key == null) {
 			throw new IllegalNullKeyException();
 		}
-		
+
 		// Check table size
-		if(numOfElements >= tableSize) {
+		if (numOfElements >= tableSize) {
 			this.resize();
 		}
-		
+
 		// Check if table is past our load threshold
-		if(numOfElements <= loadFacatorThreshold * tableSize) {
+		if (numOfElements <= loadFacatorThreshold * tableSize) {
 			hashPair<K, V> pair = new hashPair<K, V>(key, value); // new pair
-			
-			// Check for collisions, insert if none 
-			if(table[pair.hashIndex] == null) {
-				//TODO: Check for chains
+
+			// Case #01: Empty index at hash table
+			if (table[pair.hashIndex] == null) {
 				table[pair.hashIndex] = pair;
-				numOfElements++; 
+				numOfElements++;
 			}
-			
+
+			// Case #02: pair is already at hash location, must use chain/bucket(?) method
+			// TODO: make sure this actually works
 			else {
-				// Bucket(?) method to del with collisions 
-				if(table[pair.hashIndex].key == key) {
-					throw new DuplicateKeyException();
-				}
-				
 				hashPair temp = table[pair.hashIndex];
-				while(temp.chain != null) {
+				while (temp.chain != null) {
+					// Duplicate keys not allowed
+					if (temp.key == key) {
+						throw new DuplicateKeyException();
+					}
 					temp = temp.chain;
 				}
+				// Duplicate keys not allowed
+				if (temp.key == key) {
+					throw new DuplicateKeyException();
+				}
 				temp.chain = pair;
+				numOfElements++;
 			}
 		}
 	}
 
+	/**
+	 * Increases the size of table by a factor of 2 plus 1 and rehashes all the
+	 * values
+	 */
 	private void resize() {
 		// New double(ish), pseudo prime sized table
 		hashPair[] newTable = new hashPair[tableSize * 2 + 1];
 
 		// Rehashing and moving values to new table
 		for (int i = 0; i < table.length; i++) {
-			table[0].recalculateHashIndex();
-			newTable[table[0].hashIndex] = table[0];
+			if (table[i] != null) {
+				table[i].recalculateHashIndex();
+				newTable[table[i].hashIndex] = table[i];
+			}
 		}
 		table = newTable;
 	}
@@ -117,45 +130,99 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 		return null;
 	}
 
+	/**
+	 * @return number of elements in this table
+	 */
 	@Override
 	public int numKeys() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numOfElements;
 	}
 
 	/**
+	 * Returns the load factor threshold that was passed into the constructor when
+	 * creating the instance of the HashTable. When the current load factor is
+	 * greater than or equal to the specified load factor threshold, the table is
+	 * resized and elements are rehashed.
+	 * 
 	 * @return loadFactorThreshold
 	 */
+	// TODO:Fix
 	@Override
 	public double getLoadFactorThreshold() {
+		if (this.getLoadFactor() >= this.loadFacatorThreshold) {
+			this.resize();
+		}
 		return loadFacatorThreshold;
 	}
 
 	/**
-	 * @return loadFactor
+	 * @return the current load factor for this hash table, load factor = number of
+	 *         items / current table size
 	 */
 	@Override
 	public double getLoadFactor() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numOfElements / tableSize;
 	}
 
+	/**
+	 * Return the current Capacity (table size) of the hash table array.
+	 *
+	 * The initial capacity must be a positive integer, 1 or greater and is
+	 * specified in the constructor.
+	 * 
+	 * REQUIRED: When the load factor threshold is reached, the capacity must
+	 * increase to: 2 * capacity + 1
+	 *
+	 * Once increased, the capacity never decreases
+	 * 
+	 * @return the current Capacity (table size) of the hash table array.
+	 */
 	@Override
 	public int getCapacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return tableSize;
 	}
 
+	/**
+	 * Returns the collision resolution scheme used for this hash table. Implement
+	 * with one of the following collision resolution strategies. Define this method
+	 * to return an integer to indicate which strategy.
+	 * 
+	 * 1 OPEN ADDRESSING: linear probe
+	 * 
+	 * 2 OPEN ADDRESSING: quadratic probe 
+	 * 
+	 * 3 OPEN ADDRESSING: double hashing 
+	 * 
+	 * 4 CHAINED BUCKET: array list of array lists 
+	 * 
+	 * 5 CHAINED BUCKET: array list of linked lists 
+	 * 
+	 * 6 CHAINED BUCKET: array list of binary search trees 
+	 * 
+	 * 7 CHAINED BUCKET: linked list of array lists 
+	 * 
+	 * 8 CHAINED BUCKET: linked list of linked lists 
+	 * 
+	 * 9 CHAINED BUCKET: linked list of of binary search trees
+	 * 
+	 * @return collision resolution scheme used for this hash table
+	 */
 	@Override
 	public int getCollisionResolution() {
-		// TODO Auto-generated method stub
-		return 0;
+		//TODO: check if this is actually 8
+		return 8;
 	}
 
+	/**
+	 * Calculates hash index for given key
+	 * 
+	 * @param key
+	 * @return hash index
+	 */
 	private int calculateHashIndex(K key) {
 		return Math.abs(key.hashCode()) % tableSize;
 	}
-	
+
 	/**
 	 * Class for data type that will hold our key,value pair
 	 */
@@ -177,8 +244,7 @@ public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V>
 			hashIndex = Math.abs(key.hashCode()) % tableSize;
 			chain = null;
 		}
-		
-		
+
 		/**
 		 * 
 		 */
