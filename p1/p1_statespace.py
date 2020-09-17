a078c4010d43b53bc16eb384448452ad47273d10
daniel <ko28@wisc.edu>
2020-09-17 02:33:08 -0500

    hw1

--- p1/p1_statespace.py
+++ p1/p1_statespace.py
@@ -1,23 +1,37 @@
+# Name: Daniel Ko
+# Project 1, CS 540
+# Email: ko28@wisc.edu
+# Some comments were taken from the homework directly
+
+# Returns a copy of state which fills the jug corresponding to the index 
+# in which (0 or 1) to its maximum capacity. 
 def fill(state, max, which):
     temp_state = state.copy()
     temp_state[which] = max[which] 
     return temp_state
 
+# Returns a copy of state which empties the jug 
+# corresponding to the index in which (0 or 1). 
 def empty(state, max, which):
     temp_state = state.copy()
     temp_state[which] = 0
     return temp_state
 
+# Returns a copy of state which pours the contents of the jug at 
+# index source into the jug at index dest, until source is empty or dest is full
 def xfer(state, max, source, dest):
     temp_state = state.copy()
+    # Move water from source to dest until dest is full or source is empty 
     while(temp_state[dest] < max[dest] and temp_state[source] > 0):
         temp_state[dest]+=1
         temp_state[source]-=1
     return temp_state
 
+# Prints the list of unique successor states of the current state in any order.
 def succ(state, max):
     succ_states = []
     
+    # Succ states for fill and emptying 
     for i in range(len(state)):
         temp_fill = fill(state,max,i)
         if temp_fill not in succ_states:
@@ -26,7 +40,8 @@ def succ(state, max):
         temp_empty = empty(state,max,i)
         if temp_empty not in succ_states:
             succ_states.append(temp_empty) 
-    
+
+    # Succ states for transfering
     for a in range(len(state)):
         for b in range(len(state)):
             if a!=b:
--- p1/p1_weather.py
+++ p1/p1_weather.py
@@ -1,17 +1,75 @@
-def manhattan_distance(data_point1, data_point2):
-    return abs(data_point1['TMAX'] - data_point2['TMAX']) + abs(data_point1['PRCP'] - data_point2['PRCP']) + abs(data_point1['TMIN'] - data_point2['TMIN']) 
+# Name: Daniel Ko
+# Project 1, CS 540
+# Email: ko28@wisc.edu
+# Some comments were taken from the homework directly
 
+import datetime 
+
+# Distance between points in three-dimensional space, 
+# where those dimensions are the precipitation amount (PRCP), 
+# maximum temperature (TMAX), and minimum temperature for the day (TMIN)
+def manhattan_distance(data_point1, data_point2):
+    return (abs(data_point1['TMAX'] - data_point2['TMAX']) + 
+            abs(data_point1['PRCP'] - data_point2['PRCP']) +
+            abs(data_point1['TMIN'] - data_point2['TMIN'])) 
+
+# Return a list of data point dictionaries read from the specified file.
 def read_dataset(filename):
-    d = {}
+    d = [] 
     with open(filename) as f:
-        for index, line in enumerate(f):
+        for line in f:
             words = line.split()
-            d[index] = {'DATE' : words[0], 'TMAX' : float(words[1]), 'PRCP': float(words[2]), 'TMIN': float(words[3]), 'RAIN': words[4]}
+            # Generate dictionary from each line and add to list
+            d.append({'DATE': words[0], 
+                      'TMAX': float(words[1]), 
+                      'PRCP': float(words[2]), 
+                      'TMIN': float(words[3]), 
+                      'RAIN': words[4]})
     return d
 
-#dataset = read_dataset("rain.txt")
-#print(dataset[0])
-#print(len(dataset))
-print(manhattan_distance({'DATE': '1951-05-19', 'TMAX': 66.0, 'PRCP': 0.0, 'TMIN': 43.0, 'RAIN': 'FALSE'},{'DATE': '1951-01-27', 'TMAX': 33.0, 'PRCP': 0.0, 'TMIN': 19.0, 'RAIN': 'FALSE'})) 
+# Return a prediction of whether it is raining or not based on a majority vote of the list of neighbors.
+def majority_vote(nearest_neighbors):
+    numTrue = 0
+    numFalse = 0
+    for n in nearest_neighbors:
+        if n['RAIN'] == 'TRUE':
+            numTrue+=1
+        elif n['RAIN'] == 'FALSE':
+            numFalse +=1
+    
+    #If a tie occurs, default to 'TRUE' as your answer.  
+    return "TRUE" if numTrue >= numFalse else "FALSE"
 
-print(manhattan_distance({'DATE': '2015-08-12', 'TMAX': 83.0, 'PRCP': 0.3, 'TMIN': 62.0, 'RAIN': 'TRUE'}, {'DATE': '2014-05-19', 'TMAX': 70.0, 'PRCP': 0.0, 'TMIN': 50.0, 'RAIN': 'FALSE'}))
+
+def k_nearest_neighbors(filename, test_point, k, year_interval):
+    data = read_dataset(filename)
+    # adding all values within year interval from data into this list
+    within_year = []
+    test_point_date = getdate(test_point)
+    years = year_interval * datetime.timedelta(days = 365) 
+    # use date time library to do some simple subtraction on dates to determine if data point
+    # is in range
+    for d in data:
+        time_diff = test_point_date - getdate(d)
+        if years >= abs(time_diff):
+            within_year.append(d)   
+   
+    # list of tuples with manhattan_distance as first element, data point as second
+    man_dis = []
+    for d in within_year:
+        man_dis.append((manhattan_distance(test_point,d), d))
+
+    # sort this list by manhattan_distance, asending
+    # source: https://www.geeksforgeeks.org/python-program-to-sort-a-list-of-tuples-alphabetically/
+    man_dis = sorted(man_dis, key = lambda x : x[0])
+    
+    # majority_vote of closest k valid neighbors 
+    return majority_vote([x[1] for x in man_dis[:k]])
+
+# Returns datetime object given a dictionary entry of data point 
+def getdate(point):
+    datestr = point["DATE"]
+    return datetime.datetime(year = int(datestr[:4]), 
+        month = int(datestr[5:7]), day = int(datestr[8:10]))
+ 
+
