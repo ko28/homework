cb1dc5d44c93c1d78f09eae7d94f77cfd8311d52
daniel <ko28@wisc.edu>
2020-10-04 01:13:34 -0500

    wokring on P_word_given_label

--- p4/classify.py
+++ p4/classify.py
@@ -3,20 +3,42 @@
 # Email: ko28@wisc.edu
 # Some comments were taken from the homework directly
 import os 
+import math 
 
 def create_bow(vocab, filepath):
     """ Create a single dictionary for the data
         Note: label may be None
     """
     bow = {}
-    # TODO: add your code here
+    textfile = open(filepath, "r").read().split('\n')
+    for word in textfile:
+        if word in vocab:
+            if word not in bow:
+                bow[word] = 1
+            else:
+                bow[word] += 1
+        else:
+            if None not in bow:
+                bow[None] = 1
+            else:
+                bow[None] += 1
+     
 
     return bow
 
 def load_training_data(vocab, directory):
     """ Create the list of dictionaries """
     dataset = []
-    # TODO: add your code here
+    for dir in os.listdir(directory):
+        for file in os.listdir(os.path.join(directory,dir)):
+            vocab = {}
+            textfile = open(os.path.join(directory, dir, file), "r").read().split('\n')
+            for text in textfile:
+                if text not in vocab:
+                    vocab[text] = 1
+                else:
+                    vocab[text] = vocab[text] + 1
+            dataset.append({"label": dir, "bow": vocab})
 
 
     return dataset
@@ -50,17 +72,42 @@ def prior(training_data, label_list):
     smooth = 1 # smoothing factor
     logprob = {}
     # TODO: add your code here
+    count = {}
+    for label in label_list:
+        count[label] = 0
+    for data in training_data:
+        for label in label_list:
+            if data["label"] == label:
+                count[label] += 1
 
-
+    for label in label_list:
+        prob = (count[label] + smooth) / (len(training_data) + len(label_list))
+        logprob[label] = math.log(prob)
+                
     return logprob
 
 def p_word_given_label(vocab, training_data, label):
     """ return the class conditional probability of label over all words, with smoothing """
     
+
     smooth = 1 # smoothing factor
     word_prob = {}
     # TODO: add your code here
-    
+    count = {}
+    # number data["label"] == label in training data
+    num_label = 0
+    for word in vocab:
+        count[word] = 0
+        for data in training_data:
+            if data["label"] == label and word in data["bow"]:
+                    count[word] += data["bow"][word]
+    print(len(vocab) )
+    #print("len vocab ", len_vocab, "    len(vocab) ", len(vocab))
+    for word in vocab:
+        #prob = (count[word] + smooth) / (num_label)
+        #word_prob[word] = math.log(prob)
+        word_prob[word] = count[word]
+
     return word_prob
 
     
@@ -97,5 +144,17 @@ def classify(model, filepath):
 
 
 if __name__ == "__main__":
+    #vocab = create_vocabulary('./corpus/training/', 2)
+    #training_data = load_training_data(vocab,'./corpus/training/')
+    #vocab = create_vocabulary('./EasyFiles/', 2)
+    #training_data = load_training_data(vocab,'./EasyFiles/')
+    #print("training data", training_data)
+    #g = prior(training_data, ['2020', '2016'])
+    #print(g)
+
     vocab = create_vocabulary('./EasyFiles/', 1)
-    print(create_bow(vocab, './EasyFiles/2016/1.txt'))
+    training_data = load_training_data(vocab, './EasyFiles/')
+    print(training_data)
+    #print(p_word_given_label(vocab, training_data, '2020'))
+    print("len vocab ", len(vocab))
+    print(p_word_given_label(vocab, training_data, '2016'))
