172e3bdf3e1b47b74791822c30195d0a9a1cc6a6
daniel <ko28@wisc.edu>
2020-10-05 01:39:07 -0500

    아이쿠 모리아파

--- p4/classify.py
+++ p4/classify.py
@@ -4,25 +4,26 @@
 # Some comments were taken from the homework directly
 import os 
 import math 
+import pickle 
 
 def create_bow(vocab, filepath):
     """ Create a single dictionary for the data
         Note: label may be None
     """
     bow = {}
-    textfile = open(filepath, "r").read().split('\n')
-    for word in textfile:
-        if word in vocab:
-            if word not in bow:
-                bow[word] = 1
+    with open(filepath, "r") as f:
+        textfile = f.read().split('\n')
+        for word in textfile:
+            if word in vocab:
+                if word not in bow:
+                    bow[word] = 1
+                else:
+                    bow[word] += 1
             else:
-                bow[word] += 1
-        else:
-            if None not in bow:
-                bow[None] = 1
-            else:
-                bow[None] += 1
-     
+                if None not in bow:
+                    bow[None] = 1
+                else:
+                    bow[None] += 1
 
     return bow
 
@@ -31,15 +32,7 @@ def load_training_data(vocab, directory):
     dataset = []
     for dir in os.listdir(directory):
         for file in os.listdir(os.path.join(directory,dir)):
-            vocab = {}
-            textfile = open(os.path.join(directory, dir, file), "r").read().split('\n')
-            for text in textfile:
-                if text not in vocab:
-                    vocab[text] = 1
-                else:
-                    vocab[text] = vocab[text] + 1
-            dataset.append({"label": dir, "bow": vocab})
-
+            dataset.append({"label": dir, "bow": create_bow(vocab, os.path.join(directory, dir, file))})
 
     return dataset
 
@@ -50,12 +43,13 @@ def create_vocabulary(directory, cutoff):
     vocab = {}
     for root, dirs, files in os.walk(directory):
         for f in files:
-            textfile = open(os.path.join(root, f), "r").read().split('\n')
-            for text in textfile:
-                if text not in vocab:
-                    vocab[text] = 1
-                else:
-                    vocab[text] = vocab[text] + 1
+            with open(os.path.join(root, f), "r") as f:
+                textfile = f.read().split('\n')
+                for text in textfile:
+                    if text not in vocab:
+                        vocab[text] = 1
+                    else:
+                        vocab[text] = vocab[text] + 1
     
     list_vocab = []
     for key in vocab.keys():
@@ -96,18 +90,27 @@ def p_word_given_label(vocab, training_data, label):
     count = {}
     # number data["label"] == label in training data
     num_label = 0
+    vocab.append(None)
     for word in vocab:
         count[word] = 0
-        for data in training_data:
-            if data["label"] == label and word in data["bow"]:
+    for data in training_data:
+        if data["label"] == label:
+            #print(data["bow"])
+            num_label += sum(data["bow"].values())
+            for word in vocab:     
+                if word in data["bow"]:
                     count[word] += data["bow"][word]
-    print(len(vocab) )
-    #print("len vocab ", len_vocab, "    len(vocab) ", len(vocab))
-    for word in vocab:
-        #prob = (count[word] + smooth) / (num_label)
-        #word_prob[word] = math.log(prob)
-        word_prob[word] = count[word]
+    # |v|
+    v = len(vocab)
 
+    #print("num lab:", num_label, "   v:", v, "   demon:", num_label + v)
+    for word in vocab:
+        #prob = (count[word] + smooth) / (num_label + len(training_data))
+        #word_prob[word] = math.log(prob)
+        word_prob[word] = math.log(count[word] + smooth) - math.log(num_label + v)
+    
+    vocab.remove(None)
+    
     return word_prob
 
     
@@ -121,10 +124,16 @@ def train(training_directory, cutoff):
              'log p(w|y=2020)': <the output of p_word_given_label() for 2020>
             }
     """
+    
+    vocab = create_vocabulary(training_directory, cutoff)
+    training_data = load_training_data(vocab, training_directory)
+
     retval = {}
-    # TODO: add your code here
+    retval["vocabulary"] = vocab
+    retval["log prior"] = prior(training_data, ['2020', '2016'])
+    retval["log p(w|y=2016)"] = p_word_given_label(vocab, training_data, "2016")
 
-
+    retval["log p(w|y=2020)"] = p_word_given_label(vocab, training_data, "2020")
     return retval
 
 
@@ -139,22 +148,44 @@ def classify(model, filepath):
     retval = {}
     # TODO: add your code here
 
+    #retval["log p(w|y=2016)"] = p_word_given_label(model["vocab"], training_data, "2016")
 
+    #retval["log p(w|y=2020)"] = p_word_given_label(model["vocab"], training_data, "2020")
+    #vocab = model["vocab"]
+    #training_data = load_training_data(vocab, filepath)
+    #p = prior(training_data, ['2020', '2016'])
+
+    pi_2016 = 0 
+    pi_2020 = 0 
+    #print(model["log p(w|y=2016)"])
+    #exit()
+    with open(filepath, "r") as f:
+        textfile = f.read().split('\n')
+        for word in textfile:
+            if word in model["log p(w|y=2016)"]:
+                pi_2016 += model["log p(w|y=2016)"][word]
+            if word in model["log p(w|y=2020)"]:
+                pi_2020 += model["log p(w|y=2020)"][word]
+
+    print("pi2016:", pi_2016, "  model 2016:s", model["log prior"]["2016"])
+    retval["log p(w|y=2016)"] = model["log prior"]["2016"] + pi_2016
+    retval["log p(w|y=2020)"] = model["log prior"]["2020"] + pi_2020
+    #print(retval["log p(w|y=2016)"])
+    retval["predicted y"] = "2016" if retval["log p(w|y=2016)"] > retval["log p(w|y=2020)"] else "2020"
+    print(retval["predicted y"])
     return retval
 
 
 if __name__ == "__main__":
-    #vocab = create_vocabulary('./corpus/training/', 2)
-    #training_data = load_training_data(vocab,'./corpus/training/')
-    #vocab = create_vocabulary('./EasyFiles/', 2)
-    #training_data = load_training_data(vocab,'./EasyFiles/')
-    #print("training data", training_data)
-    #g = prior(training_data, ['2020', '2016'])
-    #print(g)
+    
+    with open(r"someobject.pickle", "rb") as input_file:
+        e = pickle.load(input_file)
+        print(classify(e, './corpus/test/2016/0.txt') )
+    
+    """
+    model = train('./corpus/training/', 2)
+    #print(model)
+    print(classify(model, './corpus/test/2016/0.txt') )
+    """
 
-    vocab = create_vocabulary('./EasyFiles/', 1)
-    training_data = load_training_data(vocab, './EasyFiles/')
-    print(training_data)
-    #print(p_word_given_label(vocab, training_data, '2020'))
-    print("len vocab ", len(vocab))
-    print(p_word_given_label(vocab, training_data, '2016'))
+
