e9cde39058d0c9dd3175f4490015e10c581b690c
daniel <ko28@wisc.edu>
2021-01-27 18:34:25 -0600

    fixed my-rev bug and still need to write usage

--- p1/my-look.c
+++ p1/my-look.c
@@ -1,65 +1,62 @@
+// Copyright 2021 Daniel Ko, ko@cs.wisc.edu
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-// if filename is null read from stdin 
-void look(char *search, char *filename){
-	FILE *fp;
-	if(filename == NULL){
-		fp = stdin;
-	}
-	else{
-		fp = fopen(filename, "r");
-	}
+// if filename is null read from stdin
+void look(char *search, char *filename) {
+  FILE *fp;
+  if (filename == NULL) {
+    fp = stdin;
+  } else {
+    fp = fopen(filename, "r");
+  }
 
-	if (fp == NULL) {
-		printf("my-look: cannot open file\n");
-		exit(1);
-	}
+  if (fp == NULL) {
+    printf("my-look: cannot open file\n");
+    exit(1);
+  }
 
-	
-	char buffer[100];
-	int search_len = strlen(search);
-	while(fgets(buffer, 100, fp) != NULL) {
-		if(strncasecmp(buffer, search, search_len) == 0){
-			printf("%s", buffer);
-		}
-   	}
+  char buffer[100];
+  int search_len = strlen(search);
+  while (fgets(buffer, 100, fp) != NULL) {
+    if (strncasecmp(buffer, search, search_len) == 0) {
+      printf("%s", buffer);
+    }
+  }
 
-	fclose(fp);
+  fclose(fp);
 }
 
-int main(int argc, char *argv[])
-{
-	if(argc > 1){
-		// -V : print information about this utility; 
-		if(strcmp(argv[1], "-V") == 0){
-			printf("my-look from CS537 Spring 2021\n");
-			return 0;
-		}
-		
-		// -h: prints help information about this utility
-		// TODO:
-		if(strcmp(argv[1], "-h") == 0){
-			printf("Usage: my-look \n");
-			return 0;
-		}
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    // -V : print information about this utility;
+    if (strcmp(argv[1], "-V") == 0) {
+      printf("my-look from CS537 Spring 2021\n");
+      return 0;
+    }
 
-		// specify file
-		if(strcmp(argv[1], "-f") == 0 && argc == 4){
-			look(argv[3], argv[2]);
-			return 0;
-		}
+    // -h: prints help information about this utility
+    // TODO:
+    if (strcmp(argv[1], "-h") == 0) {
+      printf("Usage: my-look \n");
+      return 0;
+    }
 
-		// stdin
-		if(argc == 2){
-			look(argv[1], NULL);
-			return 0;
-		}
+    // specify file
+    if (strcmp(argv[1], "-f") == 0 && argc == 4) {
+      look(argv[3], argv[2]);
+      return 0;
+    }
 
-	}
+    // stdin
+    if (argc == 2) {
+      look(argv[1], NULL);
+      return 0;
+    }
+  }
 
-	// unspecified commands 
-	printf("my-look: invalid command line\n");
-	return 1;
+  // unspecified commands
+  printf("my-look: invalid command line\n");
+  return 1;
 }
--- p1/my-rev.c
+++ p1/my-rev.c
@@ -1,81 +1,88 @@
+// Copyright 2021 Daniel Ko, ko@cs.wisc.edu
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-// if filename is null read from stdin 
-void rev(char *filename){
-	FILE *fp;
-	if(filename == NULL){
-		fp = stdin;
-	}
-	else{
-		fp = fopen(filename, "r");
-	}
+// if filename is null read from stdin
+void rev(char* filename) {
+  FILE* fp;
+  if (filename == NULL) {
+    fp = stdin;
+  } else {
+    fp = fopen(filename, "r");
+  }
 
-	if (fp == NULL) {
-		printf("my-rev: cannot open file\n");
-		exit(1);
-	}
+  if (fp == NULL) {
+    printf("my-rev: cannot open file\n");
+    exit(1);
+  }
 
-	
-	char buffer[100];
-	char revbuf[100];
-	memset(buffer, '\0', 100); // clear str
-	memset(revbuf, '\0', 100); // clear str
-	while(fgets(buffer, 100, fp) != NULL) {
-		// reverse string in buffer
-		int len = strlen(buffer) - 1;
-		//printf("len: %d\n", len);
-		//printf("buffer: %s", buffer);
-		for(int i = 0; i < len; i++){
-			//printf("i: %d  len-i: %d\n", i, len-i);
-			//printf("buffer[len - i]: %c \n", buffer[len-i]);
-			revbuf[i] = buffer[len - i - 1];
-			//printf("revbuf[i]: %c\n", revbuf[i]); 
-		}
-		//printf("revbuf[0]: %c\n", revbuf[0]); 
-		revbuf[len] = '\n';
-		revbuf[len + 1] = '\0';
-		printf("%s", revbuf);
-		//exit(1);
-		
-		memset(revbuf, '\0', 100); // clear string 
-   	}
+  char buffer[100];
+  char revbuf[100];
+  memset(buffer, '\0', 100);  // clear str
+  memset(revbuf, '\0', 100);  // clear str
+  while (fgets(buffer, 100, fp) != NULL) {
+    // reverse string in buffer
+    int len = strlen(buffer);
+    // int newline = 0;
+    if (buffer[len - 1] == '\n') {
+      len--;
+    }
+    // printf("len: %d\n", len);
+    // printf("buffer: %s", buffer);
+    for (int i = 0; i < len; i++) {
+      // printf("i: %d  len-i: %d\n", i, len-i);
+      // printf("buffer[len - i]: %c \n", buffer[len-i]);
+      revbuf[i] = buffer[len - i - 1];
+      // printf("revbuf[%d]: %c\n", i, revbuf[i]);
+    }
+    // printf("revbuf[0]: %c\n", revbuf[0]);
+    if (buffer[len] == '\n') {
+      revbuf[len] = '\n';
+      revbuf[len + 1] = '\0';
+    } else {
+      revbuf[len] = '\0';
+    }
+    // printf("revbuf[0]: %c\n", revbuf[0]);
 
-	fclose(fp);
+    printf("%s", revbuf);
+    // exit(1);
+
+    memset(revbuf, '\0', 100);  // clear string
+  }
+
+  fclose(fp);
 }
 
-int main(int argc, char *argv[])
-{
-	if(argc >= 1){
-		// -V : print information about this utility; 
-		if(strcmp(argv[1], "-V") == 0){
-			printf("my-rev from CS537 Spring 2021\n");
-			return 0;
-		}
-		
-		// -h: prints help information about this utility
-		// TODO:
-		if(strcmp(argv[1], "-h") == 0){
-			printf("Usage: my-rev \n");
-			return 0;
-		}
+int main(int argc, char* argv[]) {
+  if (argc >= 1) {
+    // stdin
+    if (argc == 1) {
+      rev(NULL);
+      return 0;
+    }
 
-		// specify file
-		if(strcmp(argv[1], "-f") == 0 && argc == 3){
-			rev(argv[2]);
-			return 0;
-		}
+    // -V : print information about this utility;
+    if (strcmp(argv[1], "-V") == 0) {
+      printf("my-rev from CS537 Spring 2021\n");
+      return 0;
+    }
 
-		// stdin
-		if(argc == 1){
-			rev(NULL);
-			return 0;
-		}
+    // -h: prints help information about this utility
+    // TODO:
+    if (strcmp(argv[1], "-h") == 0) {
+      printf("Usage: my-rev \n");
+      return 0;
+    }
 
-	}
+    // specify file
+    if (strcmp(argv[1], "-f") == 0 && argc == 3) {
+      rev(argv[2]);
+      return 0;
+    }
+  }
 
-	// unspecified commands 
-	printf("my-rev: invalid command line\n");
-	return 1;
+  // unspecified commands
+  printf("my-rev: invalid command line\n");
+  return 1;
 }
