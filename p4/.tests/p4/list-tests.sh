6d7f31e0df7429f10420936da7e6ed2ebbba40ff
Daniel Ko <ko28@wisc.edu>
2021-03-11 22:21:26 -0600

    wow p4 sleep no work

--- p4/.tests/p4/list-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TESTS_PATH="/u/c/s/cs537-1/tests/p4/tests"
-
-for testdesc in $( ls "$TESTS_PATH/" -v | grep ".desc" ); do
-    # echo -n $testdesc
-    echo -n "  $testdesc - "
-    echo $(head -1 $TESTS_PATH/$testdesc)
-done
